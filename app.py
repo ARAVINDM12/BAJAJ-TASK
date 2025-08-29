2327d1d05b07f648b18eb8f6681f9c5b534c0707
ARAVINDM12 <aravindm4567@gmail.com>
2025-08-29 11:50:46 +0530

    Fix app.py

--- app.py
+++ app.py
@@ -67,5 +67,8 @@ def bfhl():
             "error": str(e)
         }), 400
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
